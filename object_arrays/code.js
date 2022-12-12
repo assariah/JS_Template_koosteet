24634f36a1e6f940fce2fe3a88685273db371526
Teemu Eskola <assariah@gmail.com>
2022-12-12 10:39:16 +0200

    aa

--- object_arrays/code.js
+++ object_arrays/code.js
@@ -1,5 +1,5 @@
 //Luo tähän taulukko käyttäjiä varten.
-
+let users = [];
 document.getElementById("submit_button").addEventListener('click', submitUser);
 
 let form = document.querySelector("form");
@@ -11,13 +11,25 @@ function submitUser(e){
     e.preventDefault(); //estetään sivun päivitys, kun nappia painetaan
 
     let formData = new FormData(form);
+ //Tee tähän koodi, joka tallettaa uuden käyttäjän tiedot oliona taulukkoon
 
-    //Tee tähän koodi, joka tallettaa uuden käyttäjän tiedot oliona taulukkoon
+    let uname = formData.get("uname")
+    let password =formData.get("pw")
 
+    let user = {
+        username: uname,
+        password: pw
+    }
+    users.push(user)
+   
     //Tulostetaan aina lopuksi kaikki lisätyt käyttäjänimet
     printUsers();
 }
 
 function printUsers(){
     //Tulosta tähän kaikkien käyttäjien nimi taulukosta.
+    console.log("----------");
+    for (const user of users){
+        console.log(user.username);
+    }
 }
